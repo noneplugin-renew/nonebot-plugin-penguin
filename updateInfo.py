9282f02cab57011f2ec5a167a0b3b27731bc1b1b
Azide <rukuy@qq.com>
2022-04-10 01:51:03 +0800

    略微修正

--- updateInfo.py
+++ updateInfo.py
@@ -6,12 +6,12 @@ import time
 from tinydb import TinyDB
 
 
-def updateItemInfo(updateType:Literal['items','stages'],dbLink:str = './db/infoIdDB.json',url:str = 'https://penguin-stats.io/PenguinStats/api/v2/') -> tuple[str,str]:
+def updateItemInfo(updateType: Literal['items', 'stages'], dbLink: str = './db/infoIdDB.json', url: str = 'https://penguin-stats.io/PenguinStats/api/v2/') -> tuple[str, int]:
     '''
     - 用于更新itemId或者stageId数据库的信息，有默认形参如上
     - 返回值：(更新状态，更新时间戳)
     '''
-    infodb = TinyDB(dbLink,indent=4)
+    infodb = TinyDB(dbLink, indent=4)
     if updateType == 'items':
         try:
             itemdb = infodb.table('items')
@@ -35,9 +35,9 @@ def updateItemInfo(updateType:Literal['items','stages'],dbLink:str = './db/infoI
                 }
                 itemdb.insert(data)
         except Exception as e:
-            return repr(e),updateTime
+            return repr(e), updateTime
         else:
-            return 'OK',updateTime
+            return 'OK', updateTime
 
     elif updateType == 'stages':
         try:
@@ -64,14 +64,16 @@ def updateItemInfo(updateType:Literal['items','stages'],dbLink:str = './db/infoI
                 }
                 stagedb.insert(data)
         except Exception as e:
-            return repr(e),updateTime
+            return repr(e), updateTime
         else:
-            return 'OK',updateTime
+            return 'OK', updateTime
+
+    else:
+        return 'unknownType', 0
 
 
-
-if __name__ == '__main__' :
-    result,updatetime=updateItemInfo('items')
-    print(result,updatetime,sep='\n')
-    result,updatetime=updateItemInfo('stages')
-    print(result,updatetime,sep='\n')
+if __name__ == '__main__':
+    result, updatetime = updateItemInfo('items')
+    print(result, updatetime, sep='\n')
+    result, updatetime = updateItemInfo('stages')
+    print(result, updatetime, sep='\n')

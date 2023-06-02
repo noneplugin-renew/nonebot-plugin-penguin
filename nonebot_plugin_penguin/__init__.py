4bad8463fcba349c801a170acb35d3fd05bec983
Azide <rukuy@qq.com>
2023-06-02 16:33:36 +0800

    :arrow_up: 更新到nb 2.0.0

--- nonebot_plugin_penguin/__init__.py
+++ nonebot_plugin_penguin/__init__.py
@@ -1,9 +1,11 @@
 from nonebot import require
+from nonebot.plugin import PluginMetadata
 
 require("nonebot_plugin_apscheduler")
 require("nonebot_plugin_saa")
 require("nonebot_plugin_htmlrender")
 
+from .config import PlugConfig  # noqa： E402
 from . import (  # noqa： E402
     db,
     trim,
@@ -27,3 +29,14 @@ __all__ = [
     "types",
     "utils",
 ]
+
+__plugin_metadata__ = PluginMetadata(
+    name="nonebot_plugin_penguin",
+    description="使用nonebot2查询企鹅物流掉落物数据",
+    usage="发送命令 `penguin -h` 查看帮助",
+    type="application",
+    homepage="https://github.com/AzideCupric/nonebot-plugin-penguin",
+    config=PlugConfig,
+    supported_adapters=None,
+    extra={"authors": ["AzideCupric"], "license": "MIT"},
+)
